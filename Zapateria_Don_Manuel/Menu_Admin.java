7caff5c3128833584643c45ae7beb56249eebcef
eguachamin <151896808+eguachamin@users.noreply.github.com>
2024-07-29 00:13:34 -0500

    Commit por Evelyn

---
+++ Zapateria_Don_Manuel/Menu_Admin.java
@@ -0,0 +1,14 @@
+package Zapateria_Don_Manuel;
+
+import javax.swing.*;
+
+public class Menu_Admin {
+    private JPanel JPanel_Menu;
+    private JButton ingresarActualizarUsuariosButton;
+    private JButton eliminarUsuariosButton;
+    private JButton ventasPorVendedorButton;
+    private JButton ventasGeneralButton;
+    private JButton eliminarProductosButton;
+    private JButton ingresarActualizarProductoButton;
+    private JButton SALIRButton;
+}
