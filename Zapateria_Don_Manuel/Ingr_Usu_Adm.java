daa9c6930ed119c3fd580ab751c4fecf83ad56df
eguachamin <151896808+eguachamin@users.noreply.github.com>
2024-07-28 23:13:12 -0500

    Commit por Evelyn

---
+++ Zapateria_Don_Manuel/Ingr_Usu_Adm.java
@@ -0,0 +1,20 @@
+package Zapateria_Don_Manuel;
+
+import javax.swing.*;
+
+public class Ingr_Usu_Adm {
+    private JTextField Nom_Usuario;
+    private JTextField Apell__Usuario;
+    private JTextField Direcc_Usuario;
+    private JTextField Nac_Usuario;
+    private JTextField Telf_Usuario;
+    private JTextField UsuAs_Usuario;
+    private JTextField ContAsi_Usuario;
+    private JButton INGRESARButton;
+    private JButton MENÚButton;
+    private JTextField CI_Usuario;
+    private JButton BUSCARButton;
+    private JButton LIMPIARButton;
+    private JButton ACTUALIZARButton;
+    private JPanel JPanel_UsuAdmin;
+}
---
+++ Zapateria_Don_Manuel/Login_Admin.java
@@ -0,0 +1,13 @@
+package Zapateria_Don_Manuel;
+
+import javax.swing.*;
+
+public class Login_Admin {
+    private JTextField usuario_ing;
+    private JPasswordField pass_ing;
+    private JPanel JPanel_LogAdm;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
