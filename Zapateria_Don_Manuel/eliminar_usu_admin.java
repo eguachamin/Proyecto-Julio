0d3912b6172ba3c3daa4b79f510587ed1b19cc2c
eguachamin <151896808+eguachamin@users.noreply.github.com>
2024-07-30 00:18:08 -0500

    Commit por Evelyn

---
+++ Zapateria_Don_Manuel/eliminar_usu_admin.java
@@ -0,0 +1,19 @@
+package Zapateria_Don_Manuel;
+
+import javax.swing.*;
+
+public class eliminar_usu_admin {
+    private JPanel JPanel_eliminar;
+    private JTextField CI_Buscar;
+    private JButton BUSCARButton;
+    private JButton ELIMINARButton;
+    private JLabel name;
+    private JLabel apell;
+    private JLabel direcc;
+    private JLabel fecha_nac;
+    private JLabel telfe;
+    private JLabel usuar_asig;
+    private JLabel contra_asig;
+    private JButton MENUButton;
+    private JButton LIMPIARButton1;
+}
