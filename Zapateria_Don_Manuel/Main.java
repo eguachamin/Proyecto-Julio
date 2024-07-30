664f39ca529f2087c4ce82f7ec41c32a30e84876
eguachamin <151896808+eguachamin@users.noreply.github.com>
2024-07-30 13:50:46 -0500

    Commit por Evelyn

--- Zapateria_Don_Manuel/Main.java
+++ Zapateria_Don_Manuel/Main.java
@@ -1,4 +1,8 @@
 package Zapateria_Don_Manuel;
-
+import Zapateria_Don_Manuel_CAJERO.Login_Cajero;
 public class Main {
+    public static void main (String[] args){
+        Login_Cajero inicio = new Login_Cajero();
+        inicio.iniciar();
+    }
 }
--- Zapateria_Don_Manuel_CAJERO/Login_Cajero.java
+++ Zapateria_Don_Manuel_CAJERO/Login_Cajero.java
@@ -1,4 +1,38 @@
 package Zapateria_Don_Manuel_CAJERO;
 
-public class Login_Cajero {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login_Cajero extends JFrame{
+    private JPanel JPanel_login;
+    private JTextField user_ing;
+    private JButton INGRESARButton;
+    private JPasswordField pass_ing;
+
+    public Login_Cajero() {
+        super("Login Cajero");
+        setContentPane(JPanel_login);
+        INGRESARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String usuario_get= user_ing.getText();
+                String pass = new String(pass_ing.getPassword());
+                if (usuario_get.equals("Eve")  && pass.equals("2563-98")){
+                    JOptionPane.showMessageDialog(null,"ok");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null,"ingresaste mal tonto");
+                }
+
+            }
+        });
+    }
+    public void iniciar(){
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(500,400);
+    }
 }
